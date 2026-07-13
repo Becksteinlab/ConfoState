67fbfd1a000d2f87a9fa32912128d04fc23a7226
Joshua Raphael Uy <jauy1@asu.edu>
2026-07-12 19:27:51 -0700

    deleted weird files

--- confostate/__init__.py
+++ confostate/__init__.py
@@ -1,7 +1,3 @@
 """ConfoState package for membrane protein conformational state classification."""
 
 __version__ = "0.1.0"
-
-from confostate.data.loader import load_annotations
-
-__all__ = ["load_annotations"]
