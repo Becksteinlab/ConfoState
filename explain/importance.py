f69dba42cf73651d48a21a0b991c86308741b231
lrepa <lrepa@asu.edu>
2026-07-13 13:29:37 -0700

    example functions, advanced odd-numer solver

---
+++ explain/importance.py
@@ -0,0 +1,14 @@
+def explain(ai_results):
+    print(f'your results are {ai_results}')
+
+def isOdd(num):
+    if num == 1:
+        return True
+    elif num == 2:
+        return False
+    elif num == 3:
+        return True
+    elif num == 4:
+        return False
+    else:
+        return None
