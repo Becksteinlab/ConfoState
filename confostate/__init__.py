313a98f51f7e35c1c216e913bd4fd25928bb13c2
Oliver Beckstein <orbeckst@gmail.com>
2026-07-27 16:51:26 -0700

    ruff reformatted py files to follow coding style

--- confostate/__init__.py
+++ confostate/__init__.py
@@ -1,4 +1,4 @@
-"""ConfoState package for membrane protein conformational state classification."""
+"""ConfoState: membrane protein conformational state classification."""
 
 __version__ = "0.1.0"
 
