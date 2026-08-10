81624e608e9a8f7d3d8002d85a7e7b19a9095d97
Joshua Raphael Uy <jauy1@asu.edu>
2026-08-10 15:46:18 -0700

    used llm

--- confostate/__init__.py
+++ confostate/__init__.py
@@ -1,3 +1,8 @@
 """ConfoState: membrane protein conformational state classification."""
 
 __version__ = "0.1.0"
+
+from confostate.data.loader import load_annotations
+from confostate.features import extract_features
+
+__all__ = ["load_annotations", "extract_features"]
--- confostate/data/__init__.py
+++ confostate/data/__init__.py
@@ -1,1 +1,5 @@
 """Data loading utilities for ConfoState."""
+
+from confostate.data.loader import load_annotations, load_from_input_dir
+
+__all__ = ["load_annotations", "load_from_input_dir"]
